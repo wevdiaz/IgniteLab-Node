4334d979ad3b36d346b3be80ca5fdaac564c6d77
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2023-01-04 20:04:16 -0300

    Criando mais teste para notificações

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,7 +1,20 @@
 import { Content } from "./content"
 
-test("it should be able to create a notification content", () => {
-  const content = new Content("Você recebeu uma solicitação de amizade");
+describe("Notification content", () => {
+  it("should be able to create a notification content", () => {
+    const content = new Content("Você recebeu uma solicitação de amizade");
+  
+    expect(content).toBeTruthy();
+  });
+  
+  it("should not be able to create a notification content with less than 5 characters", () => {
+  
+    expect(() => new Content("aaa")).toThrow();
+  });
+  
+  it("should not be able to create a notification content with more than 240 characters", () => {
+  
+    expect(() => new Content("a".repeat(241))).toThrow();
+  });
+});
 
-  expect(content).toBeTruthy();
-})
