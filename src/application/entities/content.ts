d6cd09a0aa29f5d7a8d5dfdbe74aee38262e172d
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2023-01-05 18:36:03 -0300

    integrando a aplicação e testando funcionalidades em geral

--- src/application/entities/content.ts
+++ src/application/entities/content.ts
@@ -2,6 +2,10 @@
 export class Content {
   private readonly content: string;
 
+  get value(): string {
+    return this.content;
+  }
+
   private validateContentLength(content: string): boolean {
     return content.length >= 5 && content.length <= 240;
   }
